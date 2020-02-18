26e3d83d96b1e3ce0edcb36992e4e8a81d68e49b
purin52002 <purin7635@gmail.com>
2020-02-18 14:43:32 +0900

    move

--- script/JupyterUtil/args.py
+++
@@ -1,7 +0,0 @@
-from argparse import ArgumentParser
-
-
-def set_use_jupyter_args(parser: ArgumentParser):
-    parser.add_argument('--jupyter', action='store_true')
-
-    return parser
--- script/TrainDataGenerator/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='EstimateUserPreference_tournament',
-    author='sampleman',
-    license='MIT',
-)
